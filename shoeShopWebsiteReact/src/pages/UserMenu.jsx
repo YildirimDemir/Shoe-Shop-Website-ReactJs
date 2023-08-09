fb6bc7e9337052967419367255a3df81d9fa40f5
Yildirim Demir <yildirimdemir77@gmail.com>
2023-08-09 11:56:38 +0300

    User-Menu-Start

--- shoeShopWebsiteReact/src/pages/UserMenu.jsx
+++ shoeShopWebsiteReact/src/pages/UserMenu.jsx
@@ -1,5 +1,64 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function UserMenu(){
-    return <h1>User Menu</h1>
+function Button({children, onClick}){
+    return <button onClick={onClick}>{children}</button>
 }
+
+export default function UserMenu() {
+    return (
+        <div className="user-menu">
+            <div className="user-menu-left">
+                <UserInfo />
+            </div>
+        </div>
+    );
+}
+
+function UserInfo() {
+    const logouticon = "[=>";
+    return (
+        <div className="user-info">
+            <div className="user-image">
+                <div className="image">🥷🏻</div>
+            </div>
+            <div className="user-info-details">
+                <div className="user-info-detail">
+                    <input
+                     type="text"
+                     placeholder='Username'
+                     disabled
+                     />
+                    <Button>Rename</Button>
+                </div>
+                <div className="user-info-detail">
+                <input
+                     type="email"
+                     placeholder='Username'
+                     disabled
+                     />
+                    <Button>Change</Button>
+                </div>
+                <div className="user-info-detail">
+                <input
+                     type="tel"
+                     placeholder='Username'
+                     disabled
+                     />
+                    <Button>Change</Button>
+                </div>
+
+                <div className="user-details-save-btn">
+                <Button>Save</Button>
+                </div>
+            </div>
+            
+            <div className="user-menu-logout">
+                <Link to="/home">Log out {logouticon} </Link>
+            </div>
+    
+        </div>
+    );
+}
+
+
